7078d80df167dfa9fc0cf1963f1a1c000bde9c45
alesiastanciu06 <alesia.stanciu06@e-uvt.ro>
2025-11-20 13:09:12 +0000

    hello

--- Part3_Homework/10_Grading.py
+++ Part3_Homework/10_Grading.py
@@ -7,3 +7,18 @@
 # 70-79: C
 # 60-69: D
 # Below 60: F
+
+score= float(input("Enter the student's score:"))
+if score >=90:
+    grade = "A"
+elif score>= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C" 
+elif score >= 60:
+    grade = "D"
+else:
+    grade = "F"
+
+print("The student's grade is:", grade)
+
